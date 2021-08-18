f46141378a6065740f70b76384f6198a41236a92
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-08-18 17:06:26 +0200

    :(

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 
-window.onload = () => {
- getProduct()
-}
+
 
 const getProduct = async () => {
        try {
  const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
-headers: {
-    
+method: 'POST',
+body: products,
+ headers: {
+"Content-Type": "application/json",    
 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFkMGU3MTJkNTI2MjAwMTViNmRkMjEiLCJpYXQiOjE2MjkyOTQxOTMsImV4cCI6MTYzMDUwMzc5M30.XnOQr6Fx7ZY9OeWqYLc-CFjUxNxppJwzs8h_dVJmGzc"
 },
 }
 )
- if (!response.ok) throw new Error("Download failed")   
+ if (!response.ok) 
+ throw new Error("Download failed")   
  if (response.ok) {
      const data = await response.json()
      console.log(data);
@@ -23,4 +24,7 @@ headers: {
     }  
  } 
     
+ window.onload = () => {
+     getProduct().then(data => console.log(data))
+ }
 
