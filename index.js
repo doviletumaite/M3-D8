6ff2279b46ad407d7a964dabdb1514a3090aa598
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-08-18 16:22:51 +0200

    fetching data

---
+++ index.js
@@ -0,0 +1,25 @@
+productArr = []
+window.onload = () => {
+ getProduct()
+}
+
+const getProduct = async () => {
+       try {
+ const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
+headers: {
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTFkMGU3MTJkNTI2MjAwMTViNmRkMjEiLCJpYXQiOjE2MjkyOTQxOTMsImV4cCI6MTYzMDUwMzc5M30.XnOQr6Fx7ZY9OeWqYLc-CFjUxNxppJwzs8h_dVJmGzc"
+},
+}
+)
+ if (!response.ok) throw new Error("Download failed")   
+ if (response.ok) {
+     const data = await response.json()
+     console.log(data);
+ }
+}
+ catch(err) {
+        console.error(err);
+    }  
+ } 
+    
+
